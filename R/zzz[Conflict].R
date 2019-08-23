6bb509e93531c7c95edb11d6bf9b3b0affc474ac
tching <traversc@gmail.com>
2019-08-22 22:54:35 -0700

    0.18.4

--- R/zzz[Conflict].R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("qs v.0.18.3. Fixed an issue reading large S4 objects.")
-  packageStartupMessage("Objects saved in 0.18 cannot be read by earlier versions. See ChangeLog.")
-}
